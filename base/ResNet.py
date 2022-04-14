1662ef8d7ac00dac118bb343992f6401eebca480
Wu Haohua <44855029+bowu1999@users.noreply.github.com>
2022-04-14 21:58:56 +0800

    Add files via upload

---
+++ base/ResNet.py
@@ -0,0 +1,137 @@
+from torch import nn
+import torch
+from torch.nn import functional as F
+
+
+# 用于ResNet18和34的残差块，用的是2个3x3的卷积
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_channel, channel, stride=1,downsample=None):
+        super(BasicBlock, self).__init__()
+        self.downsample = downsample
+        self.conv1 = nn.Conv2d(in_channel, channel, kernel_size=3,
+                               stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(channel)
+        self.conv2 = nn.Conv2d(channel, channel, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(channel)
+        # 经过处理后的x要与x的维度相同(尺寸和深度)
+        # 如果不相同，需要添加卷积+BN来变换为同一维度
+
+    def forward(self, x):
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += identity
+        out = F.relu(out)
+        return out
+
+# todo Bottleneck
+class Bottleneck(nn.Module):
+    """
+    __init__
+        in_channel：残差块输入通道数
+        out_channel：残差块输出通道数
+        stride：卷积步长
+        downsample：在_make_layer函数中赋值，用于控制shortcut图片下采样 H/2 W/2
+    """
+    expansion = 4   # 残差块第3个卷积层的通道膨胀倍率
+    def __init__(self, in_channel, out_channel, stride=1,downsample=None ):
+        super(Bottleneck, self).__init__()
+        self.downsample = downsample
+        self.conv1 = nn.Conv2d(in_channels=in_channel, out_channels=out_channel, kernel_size=1, stride=1, bias=False)   # H,W不变。C: in_channel -> out_channel
+        self.bn1 = nn.BatchNorm2d(num_features=out_channel)
+        self.conv2 = nn.Conv2d(in_channels=out_channel, out_channels=out_channel, kernel_size=3, stride=stride, bias=False, padding=1)  # H/2，W/2。C不变
+        self.bn2 = nn.BatchNorm2d(num_features=out_channel)
+        self.conv3 = nn.Conv2d(in_channels=out_channel, out_channels=out_channel*self.expansion, kernel_size=1, stride=1, bias=False)   # H,W不变。C: out_channel -> 4*out_channel
+        self.bn3 = nn.BatchNorm2d(num_features=out_channel*self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.adaptiveAvgPool2d = nn.AdaptiveAvgPool2d((1, 1))
+        self.attention = nn.Sequential(
+            nn.Linear(in_features=out_channel*self.expansion, out_features=out_channel, bias=True),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_features=out_channel, out_features=out_channel*self.expansion, bias=True),
+            nn.Sigmoid()
+        )
+    def forward(self, x):
+        identity = x    # 将原始输入暂存为shortcut的输出
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+        out = self.conv3(out)
+        out = self.bn3(out)
+        out_avg = self.adaptiveAvgPool2d(out)
+        out_avg = torch.flatten(out_avg, 1)
+        out_scale = self.attention(out_avg)     #注意力机制
+        out_scale = out_scale.unsqueeze(2).unsqueeze(2)
+        out *= out_scale
+        out += identity     # 残差连接
+        out = self.relu(out)
+        return out
+    
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_channels = 64
+        
+        self.conv1 = nn.Conv2d(3, self.in_channels, 7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_channels)
+        self.pool1 = nn.MaxPool2d(3, 2, 1)
+        
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.avgpool2d = nn.AvgPool2d(7)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+        
+
+    def _make_layer(self, block, channel, num_blocks, stride):
+        downsample = None
+        if stride != 1 or self.in_channels != channel*block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channels, block.expansion*channel,kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(block.expansion*channel)
+            )
+        strides = [1]*(num_blocks-1)
+        layers = []
+        layers.append(block(self.in_channels, channel, stride, downsample))
+        self.in_channels = channel * block.expansion
+        for stride in strides:
+            layers.append(block(self.in_channels, channel, stride))
+            self.in_channels = channel * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.pool1(out)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.avgpool2d(out)
+        out = torch.flatten(out, 1)
+        out = self.linear(out)
+        return out
+
+def ResNet18():
+    return ResNet(BasicBlock, [2,2,2,2])
+
+def ResNet34():
+    return ResNet(BasicBlock, [3,4,6,3])
+
+def ResNet50():
+    return ResNet(Bottleneck, [3,4,6,3])
+
+def ResNet101():
+    return ResNet(Bottleneck, [3,4,23,3])
+
+def ResNet152():
+        return ResNet(Bottleneck, [3,8,36,3])
--- base/__init__.py
+++ base/__init__.py
@@ -1,1 +1,7 @@
 from .base_model import BaseModel
+
+from .ResNet import ResNet18
+from .ResNet import ResNet34
+from .ResNet import ResNet50
+from .ResNet import ResNet101
+from .ResNet import ResNet152
