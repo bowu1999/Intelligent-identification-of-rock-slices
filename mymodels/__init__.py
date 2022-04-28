9edff6f09f4e9689656b60602e7f8507fb4d727b
Wu Haohua <44855029+bowu1999@users.noreply.github.com>
2022-04-28 22:46:44 +0800

    Add files via upload

--- mymodels/__init__.py
+++ mymodels/__init__.py
@@ -1,1 +1,16 @@
-from .model import RockSlice
+# 新模型
+from .rockslice import RockSlice
+# resnet
+from .resnet import ResNet18
+from .resnet import ResNet34
+from .resnet import ResNet50
+from .resnet import ResNet101
+from .resnet import ResNet152
+# vgg
+from .vgg import VGG
+# resnext
+from .resnext import ResNeXt18
+from .resnext import ResNeXt34
+from .resnext import ResNeXt50
+from .resnext import ResNeXt101
+from .resnext import ResNeXt152
